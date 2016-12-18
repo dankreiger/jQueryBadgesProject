a09b40c554961fa0a288834cc68f6ea01e712067
Dan Kreiger <dan@dankreiger.com>
2016-12-18 13:08:26 +0100

    Complete badge project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/dankreiger.json',
+    dataType: 'jsonp',
+    success: (response) => {
+      response.courses.completed.map((course) => {
+        $('#badges').append(
+          `<div class="course">
+            <h3>${course.title}</h3>
+            <img src="${course.badge}"/>
+            <a href="${course.url}" class="btn btn-primary">See Course</a>
+          </div>`
+        )
+      });
+    }
+  })
 });
